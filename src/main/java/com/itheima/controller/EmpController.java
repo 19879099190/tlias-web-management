910a9d6d27eb49d395d5538e5eedbe9a85f2810f
wch18 <wch18979276719@163.com>
2026-08-02 21:14:48 +0800

    1

---
+++ src/main/java/com/itheima/controller/EmpController.java
@@ -0,0 +1,29 @@
+package com.itheima.controller;
+
+import com.itheima.pojo.Emp;
+import com.itheima.pojo.PageResult;
+import com.itheima.pojo.Result;
+import com.itheima.service.EmpService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//员工管理
+@RequestMapping("/emps")
+@RestController
+@Slf4j
+public class EmpController {
+
+    @Autowired
+    private EmpService empService;
+
+    //分页查询
+    @GetMapping
+    public Result page(Integer page,Integer pageSize){
+        log.info("page:{},pageSize:{}",page,pageSize);
+        PageResult<Emp> pageResult = empService.page(page, pageSize);
+        return Result.success(pageResult);
+    }
+}
